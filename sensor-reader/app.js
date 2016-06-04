bb66d0b4a240ed324b117cb490db16d4bcee48d8
Stefano Fantini <fantini.s@gmail.com>
2016-06-04 11:01:28 +0200

    adding the humidity senso module, for read the humidity data from groove humidity sensor

--- sensor-reader/app.js
+++ sensor-reader/app.js
@@ -1,4 +1,6 @@
 var temperatureSensor = require ('./sensors/temperatureSensor');
+var humiditySensor = require ('./sensors/humiditySensor');
+
 var userToken = 'a6c24dfec564ba5b602b85b981b1ad4a';
 
 
@@ -14,11 +16,24 @@ function getTemperature(interval) {
                 //call rest-server for store the data
             }
         });
-    }, interval);
-
-
-    
-    
+    }, interval); 
 };
 
+function getHumidity(interval) {
+    
+    var timer = setInterval(function(){ 
+        humiditySensor(userToken, function(err, sensorData){
+            if (err) {                
+                clearInterval(timer);
+            }
+            else {
+                console.log ('Humidity Data ' +  sensorData);
+                //call rest-server for store the data
+            }
+        });
+    }, interval); 
+};
+
+
 getTemperature(3000);
+getHumidity(3000);
---
+++ sensor-reader/sensors/humiditySensor.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var url = 'https://iot.seeed.cc/v1/node/GroveTempHumD0/humidity?access_token='
+
+module.exports = function(userToken, callback) {
+
+    request(url + userToken, function(error, response, body) {
+        if (!error && response.statusCode == 200) {            
+            
+            var sensorData = JSON.parse(body);
+            callback(null, sensorData.humidity)
+            
+        } else if (!error && response.statusCode !== 200) {            
+            
+            console.log ('Humidity sensor, response error code ' + response.statusCode);
+            callback(response.statusCode, null);
+        }
+        else {
+            
+            console.log ('Humidity sensor, rerquest error: ' + error + ' response code ' + response.statusCode);
+            callback(error, null);
+            
+        }
+
+    })
+}
